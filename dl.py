3158e62e2a4cf27f7e83520fb5efc35d0807346a
wongxy <xiyao.wong@foxmail.com>
2020-05-28 08:26:05 +0800

    .

--- dl.py
+++ dl.py
@@ -1,9 +1,12 @@
+import json
 import os
+import os.path as op
 import time
 from queue import Queue
-import json
 from threading import Thread
 
+if not op.exists('pics'):
+    os.mkdir('pics')
 
 with open('imgs.json') as f:
     imgs = json.load(f)
@@ -15,16 +18,25 @@ for img in imgs:
 def dl():
     while not img_q.empty():
         img = img_q.get()
-        os.system(f'wget {img}')
+        os.system(f'wget -P ./pics {img}')
 
 
-ts = [Thread(target=dl) for _ in range(20)]
-for i in ts:
-    i.start()
-for i in ts:
-    i.join()
-
-pics = os.listdir()
-data = {
-    'time': int(time.time()),
-    'count': }
+try:
+    ts = [Thread(target=dl) for _ in range(100)]
+    for i in ts:
+        i.start()
+    for i in ts:
+        i.join()
+except Exception as e:
+    print()
+    print(e)
+    print()
+finally:
+    pics = os.listdir('pics')
+    data = {
+        'time': int(time.time()),
+        'count': len(pics),
+        'pics': pics
+    }
+    with open('data.json', 'w') as f:
+        json.dump(data, f)
